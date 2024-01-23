df27a846a055978701867ce9494e27f7c7c67004
Tunahan Kaya <77425061+tunahankaya16@users.noreply.github.com>
2024-01-23 17:01:55 +0300

    Add project files.

---
+++ Domain/Entities/Brand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand:Entity<Guid>
+{
+    public Guid Id { get; set; }//common(ortak)
+    public string Name { get; set; }
+    public Brand()
+    {
+        
+    }
+    public Brand(Guid id,string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+
+}
