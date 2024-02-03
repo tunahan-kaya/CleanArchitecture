91833a81aaf218252ff0935fd307930280fc04cf
Tunahan Kaya <77425061+tunahankaya16@users.noreply.github.com>
2024-02-03 19:23:10 +0300

    AutoMapper Implementation

--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
 using MediatR;
 
 namespace Application.Features.Brands.Commands.Create;
 
-    public class CreateBrandCommand:IRequest<CreatedBrandResponse>
-    {
-        public string Name { get; set; }
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
 
-        public class CreateBrandCommandHandler:IRequestHandler<CreateBrandCommand,CreatedBrandResponse>
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
         {
-            public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
-            {
-                CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
-                createdBrandResponse.Name = request.Name;
-                createdBrandResponse.Id = new Guid();
-                return null;
-            }
+            _brandRepository = brandRepository;
+            _mapper = mapper;
         }
+
+        public async Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            //burada gelen request'i doğrudan döndürmüyoruz çünkü hem veri güvenliği için hem de kolonların hepsini dışarı açmamış olabiliriz
+            //veya tüm kolon verilerini almıyor olabiliriz (ki böyle olur normalde).
+            Brand brand = _mapper.Map<Brand>(request);
+            brand.Id = Guid.NewGuid();
+
+            await _brandRepository.AddAsync(brand);
+            CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(brand);
+
+            return createdBrandResponse;
+        }
+
     }
+}
 
---
+++ Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.Brands.Commands.Create;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        /*
+            eğer birisi brand ile createBrandCommand map etmek isterse onları maple.
+            ReverseMap() ile de tam tersini de mapleyebiliriz. 
+        */
+
+        CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+        CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+
+    }
+}
