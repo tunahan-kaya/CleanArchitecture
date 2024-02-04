d35c6d4c2dc03fe095777c3d2e99099b7d9cf7e5
Tunahan Kaya <77425061+tunahankaya16@users.noreply.github.com>
2024-02-04 16:50:04 +0300

    Relational Domain Modeling

--- Domain/Entities/Brand.cs
+++ Domain/Entities/Brand.cs
@@ -10,11 +10,13 @@ namespace Domain.Entities;
 public class Brand:Entity<Guid>
 {
     public string Name { get; set; }
+
+    public virtual ICollection<Model> Models { get; set; }
     public Brand()
     {
-        
+        Models = new HashSet<Model>();
     }
-    public Brand(Guid id,string name)
+    public Brand(Guid id,string name): this() // this: bu ctor çalıştığında, parametresiz ctor'u da çalıştır.
     {
         Id = id;
         Name = name;
