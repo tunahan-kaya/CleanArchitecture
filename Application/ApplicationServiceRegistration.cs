c47d6c1e2850e2e6389acb17d3b3e31915b766e0
Tunahan Kaya <77425061+tunahankaya16@users.noreply.github.com>
2024-02-07 02:35:17 +0300

    Business Rules and SubClass Type IoC implementation

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Application.Rules;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -14,15 +15,35 @@ public static class ApplicationServiceRegistration
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
+        services.AddSubClassesOfType(Assembly.GetExecutingAssembly(),typeof(BaseBusinessRules)); //BaseBusinessRules türündeki assembly'i IoC'ye ekle.
+
         services.AddMediatR(configuration =>
         {
             configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
         });
         return services;
 
-        /* 
+        /*
             Bütün assembly'i tara, orada command'leri ve query'leri bul.
             onları birbiriyle eşleştirip listene koy.
         */
+    }
+
+    //uygalama çalışmaya başladığında git assembly içerisinde BaseRules'ları bul, onları IoC'ye ekle.
+    public static IServiceCollection AddSubClassesOfType(
+       this IServiceCollection services,
+       Assembly assembly,
+       Type type,
+       Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+   )
+    {
+        var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+        foreach (var item in types)
+            if (addWithLifeCycle == null)
+                services.AddScoped(item);
+
+            else
+                addWithLifeCycle(services, type);
+        return services;
     }
 }
